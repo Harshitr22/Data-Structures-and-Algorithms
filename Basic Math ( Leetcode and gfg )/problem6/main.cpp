16925dc43f969184fa6f550527c032d606b840a3
Harshitr22 <harshitraj2212@gmail.com>
2024-07-13 23:47:56 +0100

    Add solution to problem6

--- Basic Math ( Leetcode and gfg )/problem6/main.cpp
+++ Basic Math ( Leetcode and gfg )/problem6/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+using namespace std;
+
+long long sumOfDivisors(int N)
+{
+    // Write Your Code here
+
+    long long sum=0;
+    for(int i=1; i<=N; ++i){
+
+            sum += i*(N/i);
+    }
+    return sum;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int t;
+    cin>>t;
+    cout<<sumOfDivisors(t);
     return 0;
 }
