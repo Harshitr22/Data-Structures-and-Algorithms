6a572336e9ff74fef8e696f948eddfaec2ea01ad
Harshitr22 <harshitraj2212@gmail.com>
2024-07-15 17:55:49 +0100

    Add problemr fpr coding platform

---
+++ Basic Recursion( Leetcode and gfg )/problem4/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+//Sum of first n terms
+// examples:
+// Given an integer n, calculate the sum of series 13 + 23 + 33 + 43 + … till n-th term.
+
+int sum(int n) {
+    if(n==0) return 0;
+    return n*n*n + sum(n-1);
+}
+
+int main() {
+    int t;
+    cout<<"Enter the number: ";
+    cin>>t;
+    cout<<sum(t);
+    return 0;
+}
