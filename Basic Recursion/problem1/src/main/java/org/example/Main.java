7d769ec3e3632de7ad02d376ea629079aa2a9ca1
Harshitr22 <harshitraj2212@gmail.com>
2024-07-15 15:59:54 +0100

    Add solution to problem1 java

--- Basic Recursion/problem1/src/main/java/org/example/Main.java
+++ Basic Recursion/problem1/src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
 package org.example;
 
+// Print Name N times using Recursion
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //Starting recursion topic
+        int nTime = 5;
+        print(1,nTime);
     }
+
+    public static void print(int i, int n) {
+        if(i > n) return;
+        System.out.println("Harshit");
+        print(i+1, n);
+    }
+
 }
