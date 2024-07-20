a06afa2a51d68dbae156b6bd909cfa5abe4150fe
Harshitr22 <harshitraj2212@gmail.com>
2024-07-20 14:13:26 +0100

    Add brute force approach

--- Hashing/problem3/src/main/java/org/example/Main.java
+++ Hashing/problem3/src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ public class Main{
     public static void main(String[] args){
 //        hashmaps for hashing numbers
 
-//        Getting the inputs from the user
+//        Brute force solution
         Scanner scan  = new Scanner(System.in);
         System.out.print("Enter the number of elements you want in array: ");
         int n = scan.nextInt();
@@ -19,29 +19,60 @@ public class Main{
             arr[i] = scan.nextInt();
         }
 
-        HashMap<Integer, Integer> map = find(arr);
-        System.out.print("Enter the number of element's whose count you want to know: ");
-        int q = scan.nextInt();
+//        fetching
+        System.out.print("Enter the element to be found: ");
+        int q =scan.nextInt();
+        System.out.println("The number (" + q + ") appeared " + find(arr, q) + " in an array.");
 
-        System.out.println("Enter the number of which you want to know the count: ");
 
-        while(q-- != 0){
-            int number;
-            number = scan.nextInt();
-            System.out.println("The number(" + number + ") appeared for " + map.get(number) + " times") ;
-        }
+// ----------------------------------------------------------------------------------------------
+//        Optimized solution
+//        Getting the inputs from the user
+//        Scanner scan  = new Scanner(System.in);
+//        System.out.print("Enter the number of elements you want in array: ");
+//        int n = scan.nextInt();
+//
+//        System.out.println("Enter the elements: ");
+//        int[] arr = new int[n];
+//        for(int i = 0; i < n; i++){
+//            arr[i] = scan.nextInt();
+//        }
+//
+//        HashMap<Integer, Integer> map = find(arr);
+//        System.out.print("Enter the number of elements whose count you want to know: ");
+//        int q = scan.nextInt();
+//
+//        System.out.println("Enter the number of which you want to know the count: ");
+//
+//        while(q-- != 0){
+//            int number;
+//            number = scan.nextInt();
+//            System.out.println("The number(" + number + ") appeared for " + map.get(number) + " times") ;
+//        }
     }
 
 //    Function using hashmap for numbers:
-    public static HashMap<Integer, Integer> find(int[] arr){
-        HashMap<Integer, Integer> map = new HashMap<>();
+//    public static HashMap<Integer, Integer> find(int[] arr){
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//        for(int i=0; i<arr.length; i++){
+//            int key = arr[i];
+//            int freq = 0;
+//            if(map.containsKey(key)) freq = map.get(key);
+//            freq++;
+//            map.put(key, freq);
+//        }
+//        return map;
+//    }
+
+
+//    Brute force solution
+    public static int find(int[] arr, int n){
+        int count = 0;
         for(int i=0; i<arr.length; i++){
-            int key = arr[i];
-            int freq = 0;
-            if(map.containsKey(key)) freq = map.get(key);
-            freq++;
-            map.put(key, freq);
+            if(arr[i] == n){
+                count++;
+            }
         }
-        return map;
+        return count;
     }
 }
