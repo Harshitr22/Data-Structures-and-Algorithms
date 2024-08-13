af18c33cd947b8117c15f57fc74b0e2a2a47c0a9
Harshitr22 <harshitraj2212@gmail.com>
2024-08-13 14:22:12 +0100

    new changes to problem8

--- Basic Recursion( Leetcode and gfg )/problem5/main.cpp
+++ Basic Recursion( Leetcode and gfg )/problem5/main.cpp
@@ -4,18 +4,34 @@ using namespace std;
 
 //Find all factorial numbers less than or equal to n
 
-vector<long long> factorialNumbers(long long n) {
-    // Write Your Code here
-    vector<long long> elems;
-    int factorial = 1;
-    int i=1;
-    while(factorial <= n ){
-        elems.push_back(factorial);
-        i++;
-        factorial = factorial * i;
-    }
+// vector<long long> factorialNumbers(long long n) {
+//     // Write Your Code here
+//     vector<long long> elems;
+//     int factorial = 1;
+//     int i=1;
+//     while(factorial <= n ){
+//         elems.push_back(factorial);
+//         i++;
+//         factorial = factorial * i;
+//     }
+//     return elems;
+// }
+
+// Finding the solution recursively
+vector<int> factorialNumbers(int n, int og) {
+    if(n==1) return {1};
+
+    vector<int> elems = factorialNumbers(n-1, og);
+    int currElem = n*elems.back();
+    if(currElem <= og) elems.push_back(currElem);
+
     return elems;
 }
+
+vector<int> factorialNumbers(int n) {
+    return factorialNumbers(n, n);
+}
+
 
 int main() {
     int t;
--- Hashing/problem1/src/main/java/org/example/Main.java
+++ Hashing/problem1/src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.*;
 // Number hashing
 
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 //        Brute force approach
 //        int[] arr = {1,2,1,3,2};
@@ -13,35 +13,35 @@ public class Main{
 
 
 //        Optimized approach using Hashing
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Enter the number of elements you are going to type in: ");
-        int n = scan.nextInt();
-
-        System.out.println("Enter the element of the array: ");
-        int[] arr = new int[n];
-        for(int i=0; i<n; i++){
-            arr[i] = scan.nextInt();
-        }
-
-        // pre-storing
-        int[] hash = new int[13];
-        for(int i=0; i<n; i++){
-            hash[arr[i]] += 1;
-        }
-
-        System.out.print("Enter the number of element's whose count you want to know: ");
-        int q = scan.nextInt();
-
-        System.out.println("Enter the number of which you want to know the count: ");
-
-        while(q-- != 0){
-            int number;
-            number = scan.nextInt();
-
-            System.out.println("The number(" + number + ") appeared for " + hash[number] + " times") ;
-        }
-
-    }
+//        Scanner scan = new Scanner(System.in);
+//        System.out.print("Enter the number of elements you are going to type in: ");
+//        int n = scan.nextInt();
+//
+//        System.out.println("Enter the element of the array: ");
+//        int[] arr = new int[n];
+//        for(int i=0; i<n; i++){
+//            arr[i] = scan.nextInt();
+//        }
+//
+//        // pre-storing
+//        int[] hash = new int[13];
+//        for(int i=0; i<n; i++){
+//            hash[arr[i]] += 1;
+//        }
+//
+//        System.out.print("Enter the number of element's whose count you want to know: ");
+//        int q = scan.nextInt();
+//
+//        System.out.println("Enter the number of which you want to know the count: ");
+//
+//        while(q-- != 0){
+//            int number;
+//            number = scan.nextInt();
+//
+//            System.out.println("The number(" + number + ") appeared for " + hash[number] + " times") ;
+//        }
+//
+//    }
 
 //    BRute force approach
 //    public static int find(int[] elems, int numb){
@@ -53,6 +53,30 @@ public class Main{
 //        }
 //        return count;
 //    }
+
+       boolean ans = reverse("harshit");
+       if(ans)
+       {
+           System.out.println("True");
+       }
+       else
+       {
+           System.out.println("False");
+       }
+
+    }
+
+
+//   public static boolean isPalin(String s){
+//
+//   }
+
+   public static boolean reverse(String s) {
+
+        for(int i = 0; i < s.length(); i++){
+            
+        }
+   }
 
 
 
