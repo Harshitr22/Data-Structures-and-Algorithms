b1ae6b5f249f9a6bad5ecb8c9831c1dab0453e48
Harshitr22 <harshitraj2212@gmail.com>
2024-08-13 14:53:51 +0100

    solution to problem 8 added

--- Basic Recursion( Leetcode and gfg )/problem8/src/main/java/org/example/Main.java
+++ Basic Recursion( Leetcode and gfg )/problem8/src/main/java/org/example/Main.java
@@ -1,2 +1,37 @@
 package org.example;
+import java.util.*;
 
+// Checks if the given number is the part of the fibonacci series
+
+public class Main{
+    public static void main(String[] args){
+//        Getting inout from the user
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter the number to check: ");
+        int numb = scan.nextInt();
+
+        if(isfibo(numb)){
+            System.out.println("The number is a part of fibonacci series.");
+        }else{
+            System.out.println("The number is not a part of fibonacci series.");
+        }
+    }
+
+    public static boolean isfibo(int n){
+        if(n == 1 || n ==0){
+            return true;
+        }
+        int last = 1, secondLast =0;
+        while(true){
+            int curr = secondLast + last;
+            if(curr == n){
+                return true;
+            }
+            if(curr > n){
+                return false;
+            }
+            secondLast = last;
+            last = curr;
+        }
+    }
+}
--- Basic Recursion( Leetcode and gfg )/problem8C++/main.cpp
+++ Basic Recursion( Leetcode and gfg )/problem8C++/main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 using namespace std;
 
+bool checkMember(int n){
+    if(n == 1 || n ==0){
+        return true;
+    }
+    int last = 1;
+    int secondLast =0;
+    while(true){
+        int curr = secondLast + last;
+        if(curr == n){
+            return true;
+        }
+        if(curr > n){
+            return false;
+        }
+        secondLast = last;
+        last = curr;
+    }
+}
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int numb;
+    cout<<"Enter the number: "<<endl;
+    cin>>numb;
+    bool isfibo = checkMember(numb);
+    if (isfibo) {
+        cout<<"It's a part of fibonacci series.";
+    }else {
+        cout<<"It's not a part of fibonacci series.";
+    }
 }
+
