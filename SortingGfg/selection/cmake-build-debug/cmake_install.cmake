98ee2be76b8961676fb9c553efbe7707457f9673
Harshitr22 <harshitraj2212@gmail.com>
2024-08-15 19:23:23 +0100

    corrections of folder

--- SortingGfg/selection/cmake-build-debug/cmake_install.cmake
+++ SortingGfg/selection/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/harshitraj/Documents/Striver DSA /HashingGfg/selection
+# Install script for directory: /Users/harshitraj/Documents/Striver DSA /SortingGfg/selection
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/harshitraj/Documents/Striver DSA /HashingGfg/selection/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/harshitraj/Documents/Striver DSA /SortingGfg/selection/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
